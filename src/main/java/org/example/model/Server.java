b1b42ab3293923bbb19b1b8cecd66b8ea47b883c
dariusalbu <dariusalbu3@gmail.com>
2026-04-06 22:21:20 +0300

    implemented Server.java

--- src/main/java/org/example/model/Server.java
+++ src/main/java/org/example/model/Server.java
@@ -1,13 +1,44 @@
 package org.example.model;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Server {
-    private BlockingQueue<Task> tasks;
-    private AtomicInteger waitingPeriod;
+public class Server implements Runnable {
+    private final BlockingQueue<Task> tasks;
+    private final AtomicInteger waitingPeriod;
+
+    public Server() {
+        this.tasks = new LinkedBlockingQueue<>();
+        this.waitingPeriod = new AtomicInteger(0);
+    }
 
     public void addTask(Task newTask) {
+        this.tasks.add(newTask);
+        this.waitingPeriod.addAndGet(newTask.getServiceTime());
+    }
 
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Task task = this.tasks.take();
+
+                while (task.getServiceTime() > 0) {
+                    Thread.sleep(1000);
+
+                    waitingPeriod.decrementAndGet();
+                    task.setServiceTime(task.getServiceTime() - 1);
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public BlockingQueue<Task> getTasks() {
+        return tasks;
     }
 }
