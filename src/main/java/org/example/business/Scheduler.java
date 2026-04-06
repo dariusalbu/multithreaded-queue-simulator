55d1b84e69596a6af6674fa713d4b0a59e537dc4
dariusalbu <dariusalbu3@gmail.com>
2026-04-06 22:49:37 +0300

    implemented Scheduler.java

--- src/main/java/org/example/business/Scheduler.java
+++ src/main/java/org/example/business/Scheduler.java
@@ -3,20 +3,56 @@ package org.example.business;
 import org.example.model.Server;
 import org.example.model.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scheduler {
-    private List<Server>  servers;
-    private int maxNoServers;
-    private int maxTasksPerServer;
+    private final List<Server> servers;
+    private final int maxNoServers;
+    private final int maxTasksPerServer;
     private Strategy strategy;
 
-    public void changeStrategy(SelectionPolicy selectionPolicy) {
+    public Scheduler(int maxNoServers, int maxTasksPerServer) {
+        this.maxNoServers = maxNoServers;
+        this.maxTasksPerServer = maxTasksPerServer;
+        servers = new ArrayList<Server>();
 
+        for (int i = 0; i < this.maxNoServers; i++) {
+            Server server = new Server();
+            servers.add(server);
+
+            Thread thread = new Thread(server);
+            thread.start();
+        }
+    }
+
+    public void changeStrategy(SelectionPolicy selectionPolicy) {
+        if (selectionPolicy == SelectionPolicy.SHORTEST_QUEUE) {
+            strategy = new ShortestQueueStrategy();
+        }
+        if (selectionPolicy == SelectionPolicy.SHORTEST_TIME) {
+            strategy = new ShortestTimeStrategy();
+        }
     }
 
     public void dispatchTask(Task task) {
+        strategy.addTask(servers, task);
+    }
 
+    public List<Server> getServers() {
+        return servers;
+    }
+
+    public int getMaxNoServers() {
+        return maxNoServers;
+    }
+
+    public int getMaxTasksPerServer() {
+        return maxTasksPerServer;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
     }
 
     public enum SelectionPolicy {
