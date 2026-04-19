86765fa46ad73d207ebb20bc12fbaccebbdcc834
dariusalbu <dariusalbu3@gmail.com>
2026-04-19 23:45:32 +0300

    added main components in GUI

--- src/main/java/org/example/gui/SimulationFrame.java
+++ src/main/java/org/example/gui/SimulationFrame.java
@@ -1,16 +1,24 @@
 package org.example.gui;
 
 import org.example.business.Scheduler;
+import org.example.model.Server;
 import org.example.model.SimulationData;
 import org.example.model.Task;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class SimulationFrame extends JFrame {
     private JPanel mainJPanel;
+    private JLabel waitingClientsLabel;
+    private JPanel waitingClientsJPanel;
+    private JLabel timeJLabel;
+    private JPanel queuesJPanel;
+    private JTextField timeTextField;
 
     public SimulationFrame() {
+        setContentPane(mainJPanel);
         setTitle("Simulation");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
@@ -18,7 +26,37 @@ public class SimulationFrame extends JFrame {
         setVisible(true);
     }
 
-    public void update(Scheduler scheduler, List<Task> task, SimulationData simulationData) {
+    public void update(Scheduler scheduler, List<Task> tasks, SimulationData simulationData) {
+        timeJLabel.setText("Time: " + simulationData.getCurrentTime());
 
+        waitingClientsJPanel.removeAll();
+        for (Task taskItem : tasks) {
+            JButton taskBox = new JButton(taskItem.toString());
+            waitingClientsJPanel.add(taskBox);
+        }
+
+        waitingClientsJPanel.revalidate();
+        waitingClientsJPanel.repaint();
+
+        int queueCounter = 0;
+        queuesJPanel.removeAll();
+        queuesJPanel.setLayout(new BoxLayout(queuesJPanel, BoxLayout.Y_AXIS));
+        for (Server server : scheduler.getServers()) {
+            JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            queuesJPanel.add(panel);
+
+            JButton taskBox = new JButton("Queue " + queueCounter);
+            panel.add(taskBox);
+
+            for (Task taskItem : server.getTasks()) {
+                JButton taskBox2 = new JButton(taskItem.toString());
+                panel.add(taskBox2);
+            }
+
+            queueCounter++;
+        }
+
+        queuesJPanel.revalidate();
+        queuesJPanel.repaint();
     }
 }
