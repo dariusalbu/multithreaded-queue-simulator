78a94faed1a792efccc2f06468f6d61018e894ca
dariusalbu <dariusalbu3@gmail.com>
2026-04-19 21:23:59 +0300

    working project, without gui

--- src/main/java/org/example/gui/SimulationFrame.java
+++ src/main/java/org/example/gui/SimulationFrame.java
@@ -1,4 +1,15 @@
 package org.example.gui;
 
-public class SimulationFrame {
+import javax.swing.*;
+
+public class SimulationFrame extends JFrame {
+    private JPanel mainJPanel;
+
+    public SimulationFrame() {
+        setTitle("Simulation");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
