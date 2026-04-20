1c2add8ca68f3e47d4b012c0f8bf23c1a91c839f
dariusalbu <dariusalbu3@gmail.com>
2026-04-20 20:56:50 +0300

    removed buttons from queues

--- src/main/java/org/example/gui/SimulationFrame.java
+++ src/main/java/org/example/gui/SimulationFrame.java
@@ -1,7 +1,6 @@
 package org.example.gui;
 
 import org.example.business.Scheduler;
-import org.example.model.Server;
 import org.example.model.SimulationData;
 import org.example.model.Task;
 
@@ -13,17 +12,16 @@ import java.util.List;
 
 public class SimulationFrame extends JFrame {
     private JPanel mainJPanel;
-    private JLabel waitingClientsLabel;
-    private JPanel waitingClientsJPanel;
-    private JLabel timeJLabel;
-    private JPanel queuesJPanel;
-    private JButton statisticsButton;
-    private JPanel statisticsJPanel;
-    private JLabel averageServiceTimeLabel;
-    private JLabel totalWaitingTimeLabel;
-    private JLabel peakHourLabel;
-    private JPanel showStatisticsJPanel;
-    private JTextField timeTextField;
+    private JScrollPane dataInputJPanel;
+    private JScrollPane simulationJPanel;
+    private JButton runButton;
+    private JTextField maxArrivalTimeTextField;
+    private JTextField minArrivalTimeJTextField;
+    private JTextField maxProcessingTimeJTextField;
+    private JTextField minProcessingTimeJTextField;
+    private JTextField numberOfServersJTextField;
+    private JTextField numberOfClientsJTextField;
+    private JTextField timeLimitJTextField;
 
     public SimulationFrame() {
         setContentPane(mainJPanel);
@@ -32,59 +30,23 @@ public class SimulationFrame extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+
+
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout layout = (CardLayout) mainJPanel.getLayout();
+                takeInputData();
+                layout.next(mainJPanel);
+            }
+        });
     }
 
     public void update(Scheduler scheduler, List<Task> tasks, SimulationData simulationData) {
-        timeJLabel.setText("Time: " + simulationData.getCurrentTime());
 
-        waitingClientsJPanel.removeAll();
-        for (Task taskItem : tasks) {
-            JButton taskBox = new JButton(taskItem.toString());
-            waitingClientsJPanel.add(taskBox);
-        }
+    }
 
-        waitingClientsJPanel.revalidate();
-        waitingClientsJPanel.repaint();
+    public void takeInputData() {
 
-        int queueCounter = 1;
-        boolean emptyQueues = true;
-        queuesJPanel.removeAll();
-        queuesJPanel.setLayout(new BoxLayout(queuesJPanel, BoxLayout.Y_AXIS));
-        for (Server server : scheduler.getServers()) {
-            JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            queuesJPanel.add(panel);
-
-            JButton taskBox = new JButton("Queue " + queueCounter);
-            panel.add(taskBox);
-
-            for (Task taskItem : server.getTasks()) {
-                JButton taskBox2 = new JButton(taskItem.toString());
-                panel.add(taskBox2);
-                emptyQueues = false;
-            }
-
-            queueCounter++;
-        }
-
-        queuesJPanel.revalidate();
-        queuesJPanel.repaint();
-
-        if (emptyQueues && tasks.isEmpty()) {
-            statisticsJPanel.setVisible(true);
-            statisticsButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (showStatisticsJPanel.isVisible()) {
-                        showStatisticsJPanel.setVisible(false);
-                    }
-                    else {
-                        averageServiceTimeLabel.setText("Average Service Time: " + simulationData.getCurrentTime() / simulationData.getClientsServed());
-                        totalWaitingTimeLabel.setText("Total Waiting Time: " + simulationData.getTotalWaitingTime() / simulationData.getClientsServed());
-                        peakHourLabel.setText("Peak Hour: " + simulationData.getPeakHour());
-                        showStatisticsJPanel.setVisible(true);
-                    }
-                }
-            });
-        }
     }
 }
